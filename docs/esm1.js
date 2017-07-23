bf2480175dba07091c46e8acd8300721881c7a60
teppeis <teppeis@gmail.com>
2017-07-23 14:33:11 +0900

    add named export

--- docs/esm1.js
+++ docs/esm1.js
@@ -1,1 +1,3 @@
-export default () => console.log('esm1!');
+export default () => console.log('esm1: default');
+export const foo = () => console.log('esm1: foo');
+export const bar = () => console.log('esm1: bar');
